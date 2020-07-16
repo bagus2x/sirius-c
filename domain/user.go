109ef0e4c9b4534ef5e1c1f350071e8e3084e0d7
bagus2x <bagus2x@github.com>
2020-07-16 10:13:57 -0400

    change return value

--- domain/user.go
+++ domain/user.go
@@ -7,10 +7,10 @@ import (
 // User -
 type User struct {
 	ID        primitive.ObjectID `json:"_id" bson:"_id"`
-	Username  string             `json:"Username" bson:"Username" validate:"required"`
-	Fullname  string             `json:"fullName" bson:"fullName" validate:"required"`
+	Username  string             `json:"Username" bson:"Username" validate:"required,min=4,max=50"`
+	Fullname  string             `json:"fullName" bson:"fullName" validate:"required,min=4,max=50"`
 	Email     string             `json:"email" bson:"email" validate:"required,email"`
-	Password  string             `json:"password" bson:"password" validate:"required"`
+	Password  string             `json:"password" bson:"password" validate:"required,min=8,max=20"`
 	Role      string             `json:"role" bson:"role" validate:"required"`
 	CreatedAt int64              `json:"createdAt" bson:"createdAt"`
 	UpdatedAt int64              `json:"updatedAt" bson:"updatedAt"`
@@ -20,12 +20,12 @@ type User struct {
 type UserRepository interface {
 	FindByEmailAndPassword(email string, password string) (user User, err error)
 	Create(user User) (err error)
-	GetCountByEmail(email string) (c int64, err error)
+	CountByEmail(email string) (c int64, err error)
 	// FindById(id string) User
 }
 
 // UserService -
 type UserService interface {
-	Signup(user User)
-	Signin(email string, password string)
+	Signup(user User) (err error)
+	Signin(email string, password string) (err error)
 }
