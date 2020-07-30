92a09a0a53ba371a3fa1d972dfefd0bb167b9904
bagus2x <dutapanggulu@gmail.com>
2020-07-30 08:50:53 +0700

    cleaned up

--- repositories/paper_repository.go
+++ repositories/paper_repository.go
@@ -95,19 +95,64 @@ func (pr PaperRepository) PushExamResult(id string, rst *domain.Result) (resid s
 func (pr PaperRepository) GetExamResult(id string, resid string) (res []map[string]interface{}, err error) {
 	_id, err := primitive.ObjectIDFromHex(id)
 	_resid, err := primitive.ObjectIDFromHex(resid)
+	// Mom come pick me up, i'm scared
 	if err != nil {
 		return nil, err
 	}
-	match := bson.M{"$match": bson.M{"_id": _id}}
-	unwind := bson.M{"$unwind": "$results"}
-	match2 := bson.M{"$match": bson.M{"results._id": _resid}}
-	unwind2 := bson.M{"$unwind": "$questions"}
-	unwind3 := bson.M{"$unwind": "$results.selected"}
-	redact := bson.M{"$redact": bson.M{"$cond": bson.A{bson.M{"$eq": bson.A{"$questions.qstID", "$results.selected.qstID"}}, "$$KEEP", "$$PRUNE"}}}
-	addFields := bson.M{"$addFields": bson.M{"questions.selected": "$results.selected.option", "result_id": "$results._id", "student_id": "$results.student_id"}}
-	project := bson.M{"$project": bson.M{"results": 0}}
-	group := bson.M{"$group": bson.M{"_id": bson.M{"_id": "$_id", "title": "$title", "subject": "$subject", "teacher_id": "$teacher_id", "student_id": "$student_id", "startFrom": "$startFrom", "endAt": "$endAt", "result_id": "$result_id"}, "result": bson.M{"$push": "$questions"}}}
-	project2 := bson.M{"$project": bson.M{"_id": 0, "detail": "$_id", "result": 1}}
+	match := bson.M{
+		"$match": bson.M{"_id": _id},
+	}
+	unwind := bson.M{
+		"$unwind": "$results",
+	}
+	match2 := bson.M{
+		"$match": bson.M{"results._id": _resid},
+	}
+	unwind2 := bson.M{
+		"$unwind": "$questions",
+	}
+	unwind3 := bson.M{
+		"$unwind": "$results.selected",
+	}
+	redact := bson.M{
+		"$redact": bson.M{
+			"$cond": bson.A{bson.M{
+				"$eq": bson.A{"$questions.qstID", "$results.selected.qstID"},
+			}, "$$KEEP", "$$PRUNE"},
+		},
+	}
+	addFields := bson.M{
+		"$addFields": bson.M{
+			"questions.selected": "$results.selected.option",
+			"result_id":          "$results._id",
+			"student_id":         "$results.student_id",
+		},
+	}
+	project := bson.M{
+		"$project": bson.M{"results": 0},
+	}
+	group := bson.M{
+		"$group": bson.M{
+			"_id": bson.M{
+				"_id":        "$_id",
+				"title":      "$title",
+				"subject":    "$subject",
+				"teacher_id": "$teacher_id",
+				"student_id": "$student_id",
+				"startFrom":  "$startFrom",
+				"endAt":      "$endAt",
+				"result_id":  "$result_id",
+			},
+			"result": bson.M{"$push": "$questions"},
+		},
+	}
+	project2 := bson.M{
+		"$project": bson.M{
+			"_id":    0,
+			"detail": "$_id",
+			"result": 1,
+		},
+	}
 	unwind4 := bson.M{"$unwind": "$result"}
 	group2 := bson.M{
 		"$group": bson.M{
@@ -204,8 +249,12 @@ func (pr PaperRepository) GetExamResult(id string, resid string) (res []map[stri
 							},
 						},
 					},
-				}}}}
-	curs, err := pr.db.Collection("paper").Aggregate(pr.ctx, bson.A{match, unwind, match2, unwind2, unwind3, redact, addFields, project, group, project2, unwind4, group2, addFields2})
+				},
+			},
+		},
+	}
+	pipeline := bson.A{match, unwind, match2, unwind2, unwind3, redact, addFields, project, group, project2, unwind4, group2, addFields2}
+	curs, err := pr.db.Collection("paper").Aggregate(pr.ctx, pipeline)
 	if err != nil {
 		return nil, err
 	}
